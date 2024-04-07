e7e0ffb0ed41bf8899a08046172002ed8035a4ed
dvct0 <sema.shm2@gmail.com>
2024-04-07 20:46:40 +0500

    Last commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const button = document.querySelector('press-button');
+const button = document.querySelector(".press-button");
+
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+const getRandomGradient = () => {
+  const colorStart = getRandomColor();
+  const colorEnd = getRandomColor();
+  const gradient = `linear-gradient(to right, ${colorStart}, ${colorEnd})`;
+  return gradient;
+};
+
+if (button) {
+  button.addEventListener("click", () => {
+    const gradientButton = getRandomGradient();
+    const gradientBody = getRandomGradient();
+    button.style.backgroundImage = gradientButton;
+    document.body.style.backgroundImage = gradientBody;
+  });
+}
